f815f78eeee270d237a727f706176671ed238a06
DrishttiNarwal <drishtti.narwal@gmail.com>
2025-06-05 15:46:10 +0530

    Successfully integrated telegram API and getting violence detection messages in my account

--- telegram.py
+++ telegram.py
@@ -1,10 +1,10 @@
 import requests
 
 def send_telegram_image(image_file):
-    bot_token = '<your telegram bot token>'
+    bot_token = '7668192552:AAHYh4DFY89JuzeASFY6pmC_-DunP7WWUaw'
     url = f'https://api.telegram.org/bot{bot_token}/sendPhoto'
     data = {
-        'chat_id': '<your telegram chat-id>'
+        'chat_id': '6486614215'
     }
     files = {
         'photo': image_file
@@ -13,10 +13,10 @@ def send_telegram_image(image_file):
     return response.json()
 
 def send_telegram_message():
-    bot_token = '<your telegram bot token>'
+    bot_token = '7668192552:AAHYh4DFY89JuzeASFY6pmC_-DunP7WWUaw'
     url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
     data = {
-        'chat_id': '<your telegram chat-id>',
+        'chat_id': '6486614215',
         'text': 'violence-detected'
     }
     response = requests.post(url, data=data)
