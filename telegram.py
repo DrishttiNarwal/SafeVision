6f1852038c1c42aa030c067ef08b39a3f6e803b2
DrishttiNarwal <drishtti.narwal@gmail.com>
2025-06-05 16:21:54 +0530

    Created .env and added to .gitignore to keep confidential info safe and modified telegram.py

--- telegram.py
+++ telegram.py
@@ -1,10 +1,16 @@
+import os
 import requests
 
+from dotenv import load_dotenv
+load_dotenv()  # loads environment variables from .env
+
+bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+chat_id = os.getenv('TELEGRAM_CHAT_ID')
+
 def send_telegram_image(image_file):
-    bot_token = '7668192552:AAHYh4DFY89JuzeASFY6pmC_-DunP7WWUaw'
     url = f'https://api.telegram.org/bot{bot_token}/sendPhoto'
     data = {
-        'chat_id': '6486614215'
+        'chat_id': chat_id
     }
     files = {
         'photo': image_file
@@ -13,12 +19,13 @@ def send_telegram_image(image_file):
     return response.json()
 
 def send_telegram_message():
-    bot_token = '7668192552:AAHYh4DFY89JuzeASFY6pmC_-DunP7WWUaw'
     url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
     data = {
-        'chat_id': '6486614215',
+        'chat_id': chat_id,
         'text': 'violence-detected'
     }
     response = requests.post(url, data=data)
     return response.json()
 
+print("BOT TOKEN:", bot_token)
+print("CHAT ID:", chat_id)
